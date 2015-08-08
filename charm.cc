92df0ce5d2965a36cd25fbd258c9937730b10395
陳瀛洲 <tonychen@finenet.com.tw>
2015-08-09 03:18:28 +0800

    charm.cc: 增加除錯輸出狀態列表

--- charm.cc
+++ charm.cc
@@ -31,12 +31,27 @@
    样例输出
     23
 
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+   C[] 狀態表範例: 總重量M=9, 物品N=5, (Wi,Di)=(5,8),(2,3),(4,7),(3,6),(2,4)
+
+     Wi  Di   N   0   1   2   3   4   5   6   7   8   9
+     --  --  --  --  --  --  --  --  --  --  --  --  --
+      5   8   1   -   -   -   -   -   8   8   8   8   8
+      2   3   2   -   -   3   3   3   8   8  11  11  11
+      4   7   3   -   -   3   3   7   8  10  11  11  15
+      3   6   4   -   -   3   6   7   9  10  13  14  16
+      2   4   5   -   -   4   6   7  10  11  13  14  17
+
+     註: "-" 表示初始化值零, 表示該值並未變更
+
  */
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
@@ -45,13 +60,22 @@ typedef vector<int> vint;
 static vint Wi;
 static vint Di;
 static vint CC;
+static int debug;
 
 static int dkp(int N, int M)
 {
     CC.clear();
     CC.resize(M+1, 0);
-
     int *C = &CC[0];
+
+    if (debug) {
+        cout << "Wi\tDi\tN";
+        for (int i = 0; i <= M; i++) cout << "\t" << i;
+        cout << endl << "--";
+        for (int i = M+2; i >= 0; i--) cout << "\t--";
+        cout << endl;
+    }
+
     for (int i = 0; i < N; i++) {
         // 累算第(i)個物品使用與不使用後, 各個重量(當成C[]的索引)的最大利益
 
@@ -63,6 +87,15 @@ static int dkp(int N, int M)
             // 相對遞歸版本: C[i+1][j] = max(dpk(i,j), dpk(i,j-Wi[i])+Di[i])
             C[j] = max(C[j], C[k] + Di[i]);
         }
+
+        if (debug) {
+            cout << Wi[i] << "\t" << Di[i] << "\t" << (i+1);
+            for (int i = 0; i <= M; i++) {
+                cout << "\t";
+                if (C[i]) cout << C[i]; else cout << "-";
+            }
+            cout << endl;
+        }
     }
 
     return C[M];
@@ -70,6 +103,8 @@ static int dkp(int N, int M)
 
 int main(int argc, char *argv[])
 {
+    if (argc > 1 && strncmp(argv[1], "-d", 2) == 0) debug = strlen(&argv[1][1]);
+
     int N, M;
     while (!(cin >> N >> M).eof() && N > 0 && M > 0) {
         Wi.resize(N, 0);
@@ -80,6 +115,7 @@ int main(int argc, char *argv[])
 
         int answer = dkp(N, M);
 
+        if (debug) cout << "--> ";
         cout << answer << endl;
     }
 

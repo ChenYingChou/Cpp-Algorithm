ab492855fb62108f9d16d3c1f7170daa59b1be0e
陳瀛洲 <tonychen@finenet.com.tw>
2015-08-12 01:28:55 +0800

    flip.cc: 增加輸入多樣化  、最佳化行列對掉及說明。 puzzle.cc: 加強輸入方式。

--- flip.cc
+++ flip.cc
@@ -31,24 +31,72 @@
     pieces black side up. You are to write a program that will search for the
     minimum number of rounds needed to achieve this goal.
 
-   输入
+   輸入
     The input consists of 4 lines with 4 characters "w" or "b" each that
     denote game field position.
 
-   输出
+   輸出
     Write to the output file a single integer number - the minimum number of
     rounds needed to achieve the goal of the game from the given position.
     If the goal is initially achieved, then write 0. If it's impossible to
     achieve the goal, then write the word "Impossible" (without quotes).
 
-   样例输入
+   輸入範例
     bwwb
     bbwb
     bwwb
     bwww
 
-   样例输出
+   輸出範例
     4
+
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+   資料結構:
+    每列(row)以bit-map來表示各格子(column)的值(0,1), 同時為方便翻子時的運算,
+    在最左及最右多保留1個位元。故若該列所有格子均為1, 則其值為 0111...1110b。
+
+   分析:
+    假設正在翻轉(flip)第n列時, 1~(n-2)均已解答(全部已翻轉為零), 因此要設法消
+    除第(n-1)列中的1, 這時就只有翻轉正下方第n列格子, 這樣會影響上方(目標1)、
+    自己、左方、右方及正下方。逐一將第(n-1)列的1消滅, 這樣第n列的狀態在下一
+    迴圈, 就成為決定第(n+1)翻轉的結果。最後一列狀態(由倒數第二列的狀態決定)
+    必須為零才表示有解。
+
+    以下圖例不限 4 x 4 棋盤:
+
+        ○○○○
+        ...
+        ○○○○ <- 以上列已翻轉完成(全部為零)
+        ●○●○ <- n-1列: 設法將這列的1消去, 也就是翻轉正下方的格子
+        ●○○○ <- 第n列: 正要翻轉的列, 因此要翻轉第1和3的格子
+        ○○○● <- n+1列: 本列狀態會受上列翻轉影響
+        ○●○○
+
+    由於不知第1列狀況應如何翻轉, 因此必須將第1列所有可能情況都試一次:
+        ? ? ? ?  <- 虛擬例: 從 0000b 到 1111b 全部都試一次
+        ●○○● <- 第1列: 正要翻轉的列
+        ○○○● <- 第2列: 本列狀態會受上列翻轉影響
+        ○●○●
+        ...
+        ○●○○
+
+    最後一列開始處翻轉:
+        ○○○○
+        ...
+        ○○○○ <- 以上列已翻轉完成(全部為零)
+        ●○●○ <- 倒數第2列: 設法將這列的1消去, 也就是翻轉正下方的格子
+        ●○○○ <- 最後一列: 正要翻轉的列, 因此要翻轉第1和3的格子
+        ○○○● <- 虛擬列: 本列狀態會受上列翻轉影響, 本列不會用到
+
+    最後一列翻轉後結果:
+        ? ? ? ?  <- 虛擬列: 0000b 到 1111b 的其中之一  <--------------------+
+        ○○○○                                                            |
+        ...                                                                 |
+        ○○○○ <- 以上列已翻轉完成(全部為零)                              |
+        ○○○○ <- 倒數第2列: 這列的1已全部消去                            |
+        ○○●● <- 最後一列: 翻轉後的狀態, 此即本次結果, 因含有1故失敗 ----+
+        ●○●● <- 虛擬列: 已結束用不到
 
  */
 
@@ -62,47 +110,23 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <cstring>
+#include <cctype>
 #include <assert.h>
 
 using namespace std;
 
 typedef unsigned long long ULL;
 typedef vector<ULL> vstate;
-typedef vector<char> vchar;
 
 const int MAX_SIZE = 8 * sizeof(ULL);       // bits for columns used
 
 static int debug;
 
-class Board {
-  private:
-    vstate _board;          // bit-map columns for each rows
-    vstate _press;          // flip bit-map columns for each rows
-    ULL _mask;              // bit-mask for columns: 011...110b
-    int _rows;              // 1 ~
-    int _cols;              // 1 ~ MAX_SIZE-2
-    int _min_rounds;        // minimum number of rounds
-
-    void initialize();
-    void validate(int row, int col) const;
-    void output_state(const vstate &state) const;
-    void output_press1(int row, int rounds) const;
-    void output_bits(const char *title, ULL t) const;
-    int flip_to_zero(int minRounds);
-    void flip_board();
-
-  public:
-    Board(int rows, int cols) : _rows(rows), _cols(cols) {
-        initialize();
-    }
-    Board(int size) : _rows(size), _cols(size) {
-        initialize();
-    }
-    void put_row(int row, const string &s);         // row: 1 ~ _rows
-    void output_board() const;
-    void output_press(int rounds) const;
-    int resolve();
-};
+static int bit_count(ULL x)
+{
+    bitset<MAX_SIZE> t(x);
+    return t.count();
+}
 
 static string pair_str(const char *title, int a, int b)
 {
@@ -112,31 +136,93 @@ static string pair_str(const char *title, int a, int b)
     return os.str();
 }
 
-void Board::initialize()
-{
-    if (_rows <= 0 || _cols <= 0 || _cols >= MAX_SIZE-1)
-        throw runtime_error("Invalid board "+pair_str("size", _rows, _cols));
+class Board {
+  private:
+    vstate _board;          // bit-map columns for each rows
+    vstate _press;          // flip bit-map columns for each rows
+    ULL _mask;              // bit-mask for columns: 011...110b
+    int _rotate;            // exchange (row, col)
+    int _rows;              // 1 ~
+    int _cols;              // 1 ~ MAX_SIZE-2
+    int _min_rounds;        // minimum number of rounds
 
-    _board.resize(_rows+1, 0);
+    void initialize(int rows, int cols);
+    void validate(int row, int col) const;
+    void output_state(const vstate &state) const;
+    void output_press1(int row, int rounds) const;
+    void output_bits(const char *title, ULL t) const;
+    int flip_to_zero(int minRounds);
+    void reverse_board();
+
+  public:
+    Board(int rows, int cols) { initialize(rows, cols); }
+    Board(int size) { initialize(size, size); }
+    void resize(int rows, int cols) { initialize(rows, cols); }
+    void resize(int size) { initialize(size, size); }
+    void clear();
+    void put_row(int row, const string &s);         // row: 1 ~ _rows
+    void output_board() const;
+    void output_press(int rounds) const;
+    int resolve(int mode);
+};
+
+void Board::initialize(int rows, int cols)
+{
+    _rotate = (rows < cols);
+    if (_rotate) swap(rows, cols);
+
+    if (rows <= 0 || cols <= 0 || cols >= MAX_SIZE-1)
+        throw runtime_error("Invalid board "+pair_str("size", rows, cols));
+
+    _rows = rows;
+    _cols = cols;
+    _board.resize(_rows+1);
     _mask = (1 << (_cols+1)) - 2;       // 0011...110: there are (_cols)'s 1
-    _min_rounds = 0;
+    clear();
 }
 
 void Board::validate(int row, int col) const
 {
     if (row < 1 || row > _rows || col < 1 || col > _cols)
-        throw runtime_error("Invalid board "+pair_str("cell", row, col));
+        throw runtime_error(pair_str("Cell", row, col)+" out of board");
+}
+
+void Board::clear()
+{
+    fill(_board.begin(), _board.end(), 0);
+    _press.clear();
+    _min_rounds = 0;
+}
+
+static bool is_one(char c)
+{
+    switch(tolower(c)) {
+      case '1': // One
+      case 'b': // Black
+      case 't': // True
+      case 'x': // X-mark
+        return true;
+    }
+    return false;
 }
 
 void Board::put_row(int row, const string &s)
 {
-    validate(row, s.size());
-    ULL cols = 0;
-    for (int i = s.size(); --i >= 0;) {
-        if (s[i] == '1' || s[i] == 'b' || s[i] == 'B') cols |= 1;
-        cols <<= 1;
+    if (_rotate) {
+        validate(s.size(), row);
+        int bit1 = 1 << (row+1);
+        for (int i = 0; i < s.size(); i++) {
+            if (is_one(s[i])) _board[i] |= bit1;
+        }
+    } else {
+        validate(row, s.size());
+        ULL cols = 0;
+        for (int i = s.size(); --i >= 0;) {
+            if (is_one(s[i])) cols |= 1;
+            cols <<= 1;
+        }
+        _board[row-1] = cols;
     }
-    _board[row-1] = cols;
 }
 
 void Board::output_state(const vstate &state) const
@@ -155,6 +241,7 @@ void Board::output_state(const vstate &state) const
 void Board::output_board() const
 {
     cout << "Board: " << _rows << " x " << _cols;
+    if (_rotate) cout << " (rotation)";
     output_state(_board);
 }
 
@@ -182,31 +269,34 @@ void Board::output_press(int rounds) const
     output_state(_press);
 }
 
-void Board::flip_board()
+void Board::reverse_board()
 {
     for (vstate::iterator it = _board.begin(); it != _board.end(); ++it) {
         *it = ~(*it) & _mask;
     }
 }
 
-int Board::resolve()
+// mode == 0,1,3: resolve normal board
+// mode == 0,2,3: resolve reverse board
+int Board::resolve(int mode)
 {
-    int rounds = flip_to_zero(MAX_SIZE*MAX_SIZE+1);
+    int rounds = MAX_SIZE*MAX_SIZE+1;
 
-    flip_board();
-    if (debug) output_board();
+    if (mode != 2) rounds = flip_to_zero(rounds);
 
-    rounds = min(rounds, flip_to_zero(rounds));
-    flip_board();
+    if (mode != 1) {
+        reverse_board();
+        if (debug) {
+            cout << "Reverse ";
+            output_board();
+        }
+
+        rounds = min(rounds, flip_to_zero(rounds));
+        reverse_board();
+    }
 
     _min_rounds = rounds < MAX_SIZE * MAX_SIZE ? rounds : -1;
     return _min_rounds;
-}
-
-static int bit_count(ULL x)
-{
-    bitset<MAX_SIZE> t(x);
-    return t.count();
 }
 
 int Board::flip_to_zero(int minRounds)
@@ -244,8 +334,8 @@ int Board::flip_to_zero(int minRounds)
             // row2: 下一列, 可能超出指定列數, 設定時會多加1筆, 該值無所謂
             ULL row2 = _board[n];
 
-            ULL bits_111 = 0x07;    // 111b
-            ULL bits_010 = 0x02;    // 010b
+            ULL bits_111 = 0x07;    // 111b: 左/本身/右
+            ULL bits_010 = 0x02;    // 010b: 下
             while ((row0 >>= 1) != 0) {
                 if (row0 & 1) { // 根據 row0 來決定是否翻轉對應的 row1,row2
                     row1 ^= bits_111;   // 左/本身/右
@@ -255,9 +345,9 @@ int Board::flip_to_zero(int minRounds)
                 bits_010 <<= 1;
             }
 
-            row0 = row1 & _mask;        // 下一列的解決方案: 應將 bit 為 1 的消去
+            row0 = row1 & _mask;        // 下一列的解決方案: 消去狀態為 1 的
             row1 = row2 & _mask;        // 下一列經前面步驟異動後的棋盤狀態
-            _press[n] = row0;           // _press[] 有多準備一列, 多的這列沒作用
+            _press[n] = row0;           // _press[] 有多一列, 多的這列沒用到
 
             if (debug > 1) {
                 output_press1(n, rounds);
@@ -285,41 +375,110 @@ int Board::flip_to_zero(int minRounds)
 
 //---------------------------------------------------------------------------
 
+static int number_count(const string &s)
+{
+    const char *p = s.c_str();
+    int n = 0;
+    while (*p != 0) {
+        while (isblank(*p)) p++;
+        if (*p != 0 && !isdigit(*p)) return 0;
+        while (isdigit(*p)) p++;
+        n++;
+    }
+
+    return n;
+}
+
+static void put_board(Board &B, int &row, const string &s)
+{
+    const char *p = s.c_str();
+    while (*p != 0) {
+        while (isblank(*p)) p++;
+
+        const char *t = p;
+        while (*p != 0 && !isblank(*p)) p++;
+
+        if (p == t+1) {     // assume one line: X X - X ...
+            string ss;
+            while (*t != 0) {
+                if (!isblank(*t)) ss.push_back(*t);
+                t++;
+            }
+            B.put_row(row++, ss);
+            return;
+        }
+
+        string ss(t, p-t);
+        B.put_row(row++, ss);
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    int rows = 4;
-    int cols = 4;
-    int n = 1;
-    if (n < argc && strncmp(argv[n], "-d", 2) == 0) {
-        debug = strlen(&argv[n][1]);
-        n++;
+    int rows = 0;
+    int cols = 0;
+    int mode = 0;
+
+    for (int n = 1; n < argc; n++) {
+        if (strncmp(argv[n], "-d", 2) == 0) {
+            debug += strlen(&argv[n][1]);
+        } else if (argv[n][0] == '-') {
+            mode = atoi(&argv[n][1]) & 3;
+        } else {
+            if (rows == 0) {
+                rows = atoi(argv[n]);
+            } else if (cols == 0) {
+                cols = atoi(argv[n]);
+            }
+        }
     }
-    if (n < argc) {
-        rows = atoi(argv[n]);
-        n++;
-    }
-    if (n < argc) {
-        cols = atoi(argv[n]);
-        n++;
-    }
+
+    if (rows == 0) rows = 4;
+    if (cols == 0) cols = 4;
 
     Board B(rows, cols);
-    for (int n = 1; n <= rows; n++) {
-        string s;
-        cin >> s;
-        B.put_row(n, s);
-    }
+    string s;
+    while (!getline(cin, s).eof()) {
+        if (s.empty()) continue;
 
-    if (debug) B.output_board();
+        int n = 1;
+        switch(number_count(s)) {
+          case 2:
+            {
+                stringstream os(s);
+                os >> rows >> cols;
+                B.resize(rows, cols);
+            }
+            break;
+          case 1:
+            if (s.size() == 1) continue;
+            // fall-through
+          default:
+            B.clear();
+            put_board(B, n, s);
+            break;
+        }
 
-    int answer = B.resolve();
-    if (debug) {
-        cout << "--> " << answer << endl << endl;
-    } else {
-        if (answer >= 0) {
-            cout << answer << endl;
+        while (n <= rows && !getline(cin, s).eof()) {
+            if (!s.empty()) put_board(B, n, s);
+        }
+
+        if (cin.eof()) {
+            cout << "<<< EOF >>>" << endl;
+            break;
+        }
+
+        if (debug) B.output_board();
+
+        int answer = B.resolve(mode);
+        if (debug) {
+            cout << "--> " << answer << endl << endl;
         } else {
-            cout << "Impossible" << endl;
+            if (answer >= 0) {
+                cout << answer << endl;
+            } else {
+                cout << "Impossible" << endl;
+            }
         }
     }
 
--- puzzle.cc
+++ puzzle.cc
@@ -1,4 +1,4 @@
-// puzzle.cc: 截自教材內容 -- 熄燈問題或翻格遊戲
+// puzzle.cc: 截自教材內容 -- 熄燈問題或翻格子遊戲
 //
 // 在 M x N 的棋盤中, 每一格子不是黑就是白。每當點擊某一格子時, 本身連同
 // 上下左右格子會變成另一顏色。要求以最少步驟將棋盤面上全部翻成黑色。
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ static int guess()
         step += press[1][c];
     }
 
-    // 根据press第1行和 puzzle数组, 计算press其他行的值
+    // 根據press第1行和 puzzle數組, 計算press其他行的值
     for (r = 1; r < nRow; r++) {
         for (c = 1; c <= nCol; c++) {
             int hit = (puzzle[r][c]+press[r][c]+ press[r-1][c]+
@@ -36,7 +37,7 @@ static int guess()
     }
 
     for (c = 1; c <= nCol; c++) {
-        // 判断所计算的 press数组能否熄 灭第nRow行的所有灯
+        // 判斷所計算的 press數組能否熄滅第nRow行的所有燈
         if ((press[nRow][c-1] + press[nRow][c] + press[nRow][c+1] +
             press[nRow-1][c]) % 2 != puzzle[nRow][c]) return -1;
     }
@@ -69,7 +70,7 @@ static void enumerate ()
         press[1][1]++;
         c = 1;
 
-        // 对press第1行的元素 press[1][1]~press[1][6]各种取值情况进行枚举,
+        // 對press第1行的元素 press[1][1]~press[1][6]各種取值情况進行枚舉,
         // 依次考虑:
         // 000000
         // 100000
@@ -90,6 +91,18 @@ static void enumerate ()
         cout << "Minimum step is " << minStep << endl;
     }
     return;
+}
+
+static int is_one(char c)
+{
+    switch(tolower(c)) {
+      case '1': // One
+      case 'b': // Black
+      case 't': // True
+      case 'x': // X-mark
+        return 1;
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -115,10 +128,13 @@ int main(int argc, char *argv[])
     for (i = 1; i <= cases; i++) {
         cin >> nRow >> nCol;
 
-        //读入输入数据
+        // 讀入输入數據
         for (r = 1; r <= nRow; r++) {
             for (c = 1; c <= nCol; c++) {
-                cin >> puzzle[r][c];
+                //cin >> puzzle[r][c];
+                char ch;
+                cin >> ch;
+                puzzle[r][c] = is_one(ch);
             }
         }
 

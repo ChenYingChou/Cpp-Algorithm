03dc79c91b0003522d57d8b8efaf5d19b4d5bfb9
陳瀛洲 <tonychen@finenet.com.tw>
2015-08-11 12:19:32 +0800

    flip.cc: 修正錯誤及增加說明

--- flip.cc
+++ flip.cc
@@ -86,8 +86,9 @@ class Board {
     void initialize();
     void validate(int row, int col) const;
     void output_state(const vstate &state) const;
-    void output_press1(int nth, int rounds) const;
-    int flip_to_zero();
+    void output_press1(int row, int rounds) const;
+    void output_bits(const char *title, ULL t) const;
+    int flip_to_zero(int minRounds);
     void flip_board();
 
   public:
@@ -116,7 +117,7 @@ void Board::initialize()
     if (_rows <= 0 || _cols <= 0 || _cols >= MAX_SIZE-1)
         throw runtime_error("Invalid board "+pair_str("size", _rows, _cols));
 
-    _board.resize(_rows, 0);
+    _board.resize(_rows+1, 0);
     _mask = (1 << (_cols+1)) - 2;       // 0011...110: there are (_cols)'s 1
     _min_rounds = 0;
 }
@@ -157,15 +158,21 @@ void Board::output_board() const
     output_state(_board);
 }
 
-void Board::output_press1(int nth, int rounds) const
+void Board::output_bits(const char *title, ULL t) const
 {
-    cout << "[" << nth << "]: " << setw(2) << rounds;
-    ULL t = _press[nth];
+    cout << title;
     for (int j = 0; j < _cols; j++) {
-        cout << ' ';
         t >>= 1;
-        cout << (t & 1 ? 'X' : '-');
+        cout << ' ' << (t & 1 ? 'X' : '-');
     }
+    cout << " |";
+}
+
+void Board::output_press1(int row, int rounds) const
+{
+    cout << "[" << setfill('0') << setw(2) << row << "]: "
+        << setfill(' ') << setw(2) << rounds;
+    output_bits(" |", _press[row-1]);
     cout << endl;
 }
 
@@ -184,15 +191,16 @@ void Board::flip_board()
 
 int Board::resolve()
 {
-    int rounds = flip_to_zero();
+    int rounds = flip_to_zero(MAX_SIZE*MAX_SIZE+1);
 
     flip_board();
     if (debug) output_board();
 
-    rounds = min(rounds, flip_to_zero());
+    rounds = min(rounds, flip_to_zero(rounds));
     flip_board();
 
-    return rounds;
+    _min_rounds = rounds < MAX_SIZE * MAX_SIZE ? rounds : -1;
+    return _min_rounds;
 }
 
 static int bit_count(ULL x)
@@ -201,53 +209,63 @@ static int bit_count(ULL x)
     return t.count();
 }
 
-int Board::flip_to_zero()
+int Board::flip_to_zero(int minRounds)
 {
-    _min_rounds = MAX_SIZE * MAX_SIZE + 1;
-    _press.resize(_rows);
+    _press.resize(_rows+1);
 
     // 將 row[0] 每一狀況都試一次: 2^(_cols)
     ULL flip0 = 1 << _cols;
     do {
         flip0--;
+        if (bit_count(flip0) >= minRounds) continue;
+
+        int rounds = 0;
+        ULL row0 = flip0 << 1;          // 前一列狀態: 要設法將之變為零
+        ULL row1 = _board[0];           // 即將處理這列棋盤狀態
+
         fill(_press.begin(), _press.end(), 0);
+        _press[0] = row0;               // 第一列預期的點擊結果
 
-        int rounds = bit_count(flip0);
-        _press[0] = flip0 << 1;
-        if (debug > 1) output_press1(0, rounds);
+        for (int n = 1; n <= _rows; n++) {
+            // 預判這一列點擊次數, 若累計超過目前最小次數, 則換 row[0] 下一狀況
+            rounds += bit_count(row0);
+            if (rounds >= minRounds) {
+                if (debug > 1) {
+                    output_press1(n, rounds);
+                    cout << "*** break: " << rounds << endl;
+                }
+                row0 = ~0;
+                break;
+            }
 
-        ULL row0 = _board[0] ^ (flip0 << 1);        // new bit-map for row[0]
-        ULL row1 = _rows > 1 ? _board[1] : 0;
-
-        for (int n = 1; n < _rows; n++) {
             // 試著將 row0 所有 bit 為 1 者變換為 0
             // 將對應 row1 翻轉, 影響: 自身左右(row1)/上(row0)/下(row2) 狀態
-            ULL row2 = n+1 < _rows ? _board[n+1] : 0;
 
-            ULL flip1 = 0;
-            ULL bits_111 = 0x03;
-            ULL bits_010 = 0x02;
+            // row2: 下一列, 可能超出指定列數, 設定時會多加1筆, 該值無所謂
+            ULL row2 = _board[n];
+
+            ULL bits_111 = 0x07;    // 111b
+            ULL bits_010 = 0x02;    // 010b
             while ((row0 >>= 1) != 0) {
                 if (row0 & 1) { // 根據 row0 來決定是否翻轉對應的 row1,row2
-                    flip1 |= bits_010;  // row1 == _press[n]: flip 狀態
-                    row1  ^= bits_111;  // 左/本身/右
-                    row2  ^= bits_010;  // 下
-                    rounds++;
-                    if (rounds >= _min_rounds) break;
+                    row1 ^= bits_111;   // 左/本身/右
+                    row2 ^= bits_010;   // 下
                 }
                 bits_111 <<= 1;
                 bits_010 <<= 1;
             }
 
-            row0 = row1 & _mask;
-            row1 = row2 & _mask;
-            _press[n] = flip1;
-            if (debug > 1) output_press1(n, rounds);
+            row0 = row1 & _mask;        // 下一列的解決方案: 應將 bit 為 1 的消去
+            row1 = row2 & _mask;        // 下一列經前面步驟異動後的棋盤狀態
+            _press[n] = row0;           // _press[] 有多準備一列, 多的這列沒作用
 
-            if (rounds >= _min_rounds) {
-                row0 = ~0;
-                if (debug > 1) cout << "*** break ***" << endl;
-                break;
+            if (debug > 1) {
+                output_press1(n, rounds);
+                if (debug > 2) {
+                    output_bits("         |", row0);
+                    output_bits("", row1);
+                    cout << endl;
+                }
             }
         }
 
@@ -255,14 +273,14 @@ int Board::flip_to_zero()
 
         if (row0 == 0) {
             // 全部翻成白子
-            if (rounds < _min_rounds) {
-                _min_rounds = rounds;
+            if (rounds < minRounds) {
+                minRounds = rounds;
                 if (debug) output_press(rounds);
             }
         }
     } while (flip0 > 0);
 
-    return _min_rounds;
+    return minRounds;
 }
 
 //---------------------------------------------------------------------------
@@ -298,7 +316,11 @@ int main(int argc, char *argv[])
     if (debug) {
         cout << "--> " << answer << endl << endl;
     } else {
-        cout << answer << endl;
+        if (answer >= 0) {
+            cout << answer << endl;
+        } else {
+            cout << "Impossible" << endl;
+        }
     }
 
     return 0;

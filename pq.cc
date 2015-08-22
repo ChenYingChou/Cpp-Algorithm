b3bc3f8200fa285b95f9d573c80864bae010cee1
陳瀛洲 <tonychen@finenet.com.tw>
2015-08-22 15:25:15 +0800

    將相關質數用到的資料型態改為無正負號, 並改寫乘法取餘數函式。調整測試質數處理, 若不是質數則輸出其質因數分解結果。

--- pq.cc
+++ pq.cc
@@ -31,6 +31,8 @@
 #include <queue>
 #include <cstring>
 #include <cstdlib>
+#include <cstdint>
+#include <cctype>
 #include <assert.h>
 
 using namespace std;
@@ -42,18 +44,20 @@ using namespace std;
 typedef vector<unsigned int> vuint;
 
 const unsigned int MAX_NUM = 10000000;
-static vuint Prime;                         // not include 2: 3, 5, 7, 11 ...
+
+static vuint Primes;                        // not include 2: 3, 5, 7, 11 ...
 static int debug;
 
 //---------------------------------------------------------------------------
 
 class Node {
   private:
-    int _value;                             // 數值
-    int _num;                               // 質因數個數
+    unsigned int _value;                    // 數值
+    unsigned int _num;                      // 質因數個數
     bool _used;                             // 已取出過
   public:
-    Node(int value, int num) : _value(value), _num(num), _used(false) {
+    Node(unsigned int value, unsigned int num)
+    : _value(value), _num(num), _used(false) {
         if (debug > 2) {
             cout << "--> Node(): " << this
                 << " (" << _value
@@ -72,8 +76,8 @@ class Node {
         }
         assert(_used);
     }
-    int value() const { return _value; }
-    int num() const { return _num; }
+    unsigned int value() const { return _value; }
+    unsigned int num() const { return _num; }
     bool used() const { return _used; }
     void set_used() { _used = true; }
 };
@@ -81,8 +85,8 @@ class Node {
 typedef Node * pNode;
 
 bool operator< (const Node &x, const Node &y) {
-    int sts = x.num() - y.num();
-    return sts ? (sts < 0) : (x.value() < y.value());
+    if (x.num() != y.num()) return x.num() < y.num();
+    return x.value() < y.value();
 }
 
 class CmpMaxMin {
@@ -101,37 +105,37 @@ class PQ_MaxMin {
   private:
     pqMaxMin _pqMax;
     pqMaxMin _pqMin;
-    bool get(pqMaxMin &pq, int &value);
+    bool get(pqMaxMin &pq, unsigned int &value);
   public:
     PQ_MaxMin() : _pqMin(CmpMaxMin(true)) {}
     ~PQ_MaxMin();
-    void push(int value, int num);
-    void pop(int &maxValue, int &minValue);
+    void push(unsigned int value, unsigned int num);
+    void pop(unsigned int &maxValue, unsigned int &minValue);
 };
 
 PQ_MaxMin::~PQ_MaxMin()
 {
     if (debug > 2) cout << ">>> ~PQ_MaxMin(): Release _pqMax()" << endl;
     while (!_pqMax.empty()) {
-        int value;
+        unsigned int value;
         get(_pqMax, value);
     }
 
     if (debug > 2) cout << endl << ">>> ~PQ_MaxMin(): Release _pqMin()" << endl;
     while (!_pqMin.empty()) {
-        int value;
+        unsigned int value;
         get(_pqMin, value);
     }
 }
 
-void PQ_MaxMin::push(int value, int num)
+void PQ_MaxMin::push(unsigned int value, unsigned int num)
 {
     pNode p = new Node(value, num);
     _pqMax.push(p);
     _pqMin.push(p);
 }
 
-bool PQ_MaxMin::get(pqMaxMin &pq, int &value)
+bool PQ_MaxMin::get(pqMaxMin &pq, unsigned int &value)
 {
     while (!pq.empty()) {
         pNode p = pq.top();
@@ -146,7 +150,7 @@ bool PQ_MaxMin::get(pqMaxMin &pq, int &value)
     return false;
 }
 
-void PQ_MaxMin::pop(int &maxValue, int &minValue)
+void PQ_MaxMin::pop(unsigned int &maxValue, unsigned int &minValue)
 {
     assert(get(_pqMax, maxValue));
     assert(get(_pqMin, minValue));
@@ -160,36 +164,47 @@ T modpow(T base, T exp, T modulus) {
   base %= modulus;
   T result = 1;
   while (exp > 0) {
-    if (exp & 1) result = ((unsigned long long)result * base) % modulus;
-    base = ((unsigned long long)base * base) % modulus;
+    if (exp & 1) result = (result * base) % modulus;    // <-- overflow
+    base = (base * base) % modulus;                     // <-- overflow
     exp >>= 1;
   }
   return result;
 }
 */
 
+// (a * b) % n
+static uint64_t MULMOD(uint64_t a, uint64_t b, uint64_t n) {
+    if (a < b) swap(a, b);
+    if (a <= UINT64_MAX / b) return a * b % n;
+    uint64_t result = 0;
+    while (b) {
+        if (b & 1) {
+            result += a;
+            if (result < a || result >= n) result -= n;
+        }
+        uint64_t t = a;
+        a += a;
+        if (a < t || a >= n) a -= n;
+        b >>= 1;
+    }
+    return result;
+}
+
 // (base ^ exp) mod modulus
-unsigned int POWMOD(unsigned int base, unsigned int exp, unsigned int modulus) {
+static uint64_t POWMOD(uint64_t base, uint64_t exp, uint64_t modulus) {
   base %= modulus;
-  unsigned int result = 1;
+  uint64_t result = 1;
   while (exp > 0) {
-    if (exp & 1) result = ((unsigned long long)result * base) % modulus;
-    base = ((unsigned long long)base * base) % modulus;
+    if (exp & 1) result = MULMOD(result, base, modulus);
+    base = MULMOD(base, base, modulus);
     exp >>= 1;
   }
   return result;
 }
 
-// ( x * y) mod n
-static inline unsigned int MUL(unsigned int x, unsigned int y, unsigned int n)
-{
-    unsigned long long z = (unsigned long long)x * y;
-    return z % n;
-}
-
 static bool SPRP_is_prime(unsigned int n)
 {
-    // 預先判斷偶數與1，節省一點時間。
+    // 預先判斷偶數與1, 節省一點時間
     if (n <= 2 || (n & 1) == 0) return n == 2;
 
     unsigned int u = n - 1, t = 0;
@@ -198,13 +213,13 @@ static bool SPRP_is_prime(unsigned int n)
         t++;
     }
 
-    // 推定是質數，就實施下一次測試；
-    // 確定是合數，就馬上結束。
-    unsigned int sprp[3] = {2, 7, 61};   // 足以涵蓋int32範圍
+    // 推定是質數，就實施下一次測試
+    // 確定是合數，就馬上結束
+    unsigned int sprp[3] = {2, 7, 61};   // 足以涵蓋 2^32 範圍
     for (int k = 0; k < 3; k++) {
-        // a沒有大於1、小於n-1的限制，
-        // 沒有測試意義的數字，當作是通過測試。
-        // （a是質數時，模n後不會等於零，不必特別判斷。）
+        // a沒有大於1、小於n-1的限制
+        // 沒有測試意義的數字，當作是通過測試
+        // a是質數時, 除以n的餘數一定不是零, 不必特別判斷
         unsigned int a = sprp[k] % n;
         if (a == 0 || a == 1 || a == n-1) continue;
 
@@ -212,7 +227,7 @@ static bool SPRP_is_prime(unsigned int n)
         if (x == 1 || x == n-1) continue;
 
         for (int i = 0; i < t-1; i++) {
-            x = MUL(x, x, n);
+            x = MULMOD(x, x, n);
             if (x == 1) return false;
             if (x == n-1) break;
         }
@@ -220,7 +235,8 @@ static bool SPRP_is_prime(unsigned int n)
 
         return false;
     }
-    // 通過全部測試，確定是質數。
+
+    // 通過全部測試，確定是質數
     return true;
 }
 
@@ -229,8 +245,8 @@ static bool SPRP_is_prime(unsigned int n)
 static bool is_prime(unsigned int n)
 {
     if ((n & 1) == 0 || n < 2) return n == 2;
-    for (int i = 0, k = Prime.size(); i < k; i++) {
-        unsigned int m = Prime[i];
+    for (int i = 0, k = Primes.size(); i < k; i++) {
+        unsigned int m = Primes[i];
         if (m * m > n) break;
         if (n % m == 0) return false;
     }
@@ -239,15 +255,16 @@ static bool is_prime(unsigned int n)
 
 //---------------------------------------------------------------------------
 
-// 求 n 的質因數個數:
-// 252 = 2^2 x 3^3 x 7 => (2,3,7) => 252 有 3 個質因數
-// 221 = 13 x 17 => 2個
-static int factor_prime(unsigned int n)
+// 將 n 做質因數分解, 返回質因數個數:
+// 252 = 2^2 × 3^3 × 7 => (2,3,7) => 252 有 3 個質因數
+// 221 = 13 × 17 => 2個質因數
+// 開啟除錯模式(執行參數 -d)可輸出分解的質因數
+static unsigned int prime_factoring(unsigned int n)
 {
-    int count = 0;
+    unsigned int count = 0;
     if ((n & 1) == 0) {     // even
         count++;
-        int cnt = 1;
+        unsigned int cnt = 1;
         while (n && ((n >>= 1) & 1) == 0) cnt++;
         if (debug) {
             cout << " 2";
@@ -255,18 +272,18 @@ static int factor_prime(unsigned int n)
         }
     }
 
-    for (int i = 0, k = Prime.size(); i < k; i++) {
-        unsigned int m = Prime[i];
+    for (unsigned int i = 0, k = Primes.size(); i < k; i++) {
+        unsigned int m = Primes[i];
         if (m >= n) {
             if (m == n) break;
             return count;
         }
         if (n % m == 0) {
             count++;
-            int cnt = 1;
+            unsigned int cnt = 1;
             while ((n /= m) && n % m == 0) cnt++;
             if (debug) {
-                cout << ' ' << m;
+                cout << (count == 1 ? " " : " × ") << m;
                 if (cnt > 1) cout << '^' << cnt;
             }
         }
@@ -274,7 +291,7 @@ static int factor_prime(unsigned int n)
 
     if (count) {    // 必須是因子才要計數, 本身是質數則不算
         count++;
-        if (debug) cout << ' ' << n;
+        if (debug) cout << (count == 1 ? " " : " × ")  << n;
     }
     return count;
 }
@@ -283,32 +300,37 @@ static int factor_prime(unsigned int n)
 
 static bool (*isPrime)(unsigned int) = is_prime;
 
-static void run()
+static void fill_primes(unsigned int max_num)
 {
-    Prime.clear();
-    Prime.reserve(512);
+    Primes.clear();
+    Primes.reserve(max_num / 10);
 
     if (debug > 1) cout << "Primes: 2 ";
-    for (unsigned int i = 3; i*i < MAX_NUM; i++) {
+    for (unsigned int i = 3; i <= max_num / i; i++) {
         if (isPrime(i)) {
-            Prime.push_back(i);
+            Primes.push_back(i);
             if (debug > 1) cout << i << ' ';
         }
     }
-    if (debug > 1) cout << endl << "--> "<< Prime.size()+1 << endl << endl;
+    if (debug > 1) cout << endl << "--> "<< Primes.size()+1 << endl << endl;
+}
+
+static void run(unsigned int max_num)
+{
+    if (Primes.size() == 0) fill_primes(max_num);
 
     PQ_MaxMin pq;
-    int num;
+    unsigned int num;
 
     cin >> num;
     for (int i = 0; i < num; i++) {
         for (int j = 0; j < 10; j++) {
-            int value;
+            unsigned int value;
             cin >> value;
-            assert(0 < value && value < MAX_NUM);
+            assert(value < max_num);
 
             if (debug) cout << "--> " << value << ":";
-            int fpNum = factor_prime(value);
+            unsigned int fpNum = prime_factoring(value);
             if (debug) cout << " => " << fpNum << endl;
 
             pq.push(value, fpNum);
@@ -316,7 +338,7 @@ static void run()
 
         if (debug) cout << endl;
 
-        int maxValue, minValue;
+        unsigned int maxValue, minValue;
         pq.pop(maxValue, minValue);
         cout << maxValue << ' ' << minValue << endl;
 
@@ -328,36 +350,52 @@ int main (int argc, char *argv[])
 {
     std::ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    unsigned int max_num = MAX_NUM;
     {
         int n = 1;
         while (n < argc && argv[n][0] == '-') {
             if (strncmp(argv[n], "-d", 2) == 0) {
                 debug += strlen(&argv[n][1]);
-            } else if (strncmp(argv[n], "-sprp", min(5, (int)strlen(argv[n]))) == 0) {
+            } else if (strncmp(argv[n], "-s", 2) == 0 ||
+                       strncmp(argv[n], "-p", 2) == 0) {
                 isPrime = SPRP_is_prime;
+            } else if (strncmp(argv[n], "-m", 2) == 0) {
+                char *p = &argv[n][2];
+                while (*p && !isdigit(*p)) p++;
+                if (isdigit(*p)) {
+                    max_num = atoi(p);
+                    if (max_num < 10) max_num = MAX_NUM;
+                }
             } else if (strncmp(argv[n], "-i:", 3) == 0) {
                 ifstream in(&argv[n][3]);
                 //streambuf *cinbuf = cin.rdbuf(); //save old buf
                 cin.rdbuf(in.rdbuf()); //redirect std::cin to in.txt!
-                run();
+                run(max_num);
                 //cin.rdbuf(cinbuf);
                 return 0;
             }
             n++;
         }
-        if (n < argc) {
+        if (n < argc) {     // testing whether a number is prime
+            fill_primes(UINT_MAX);
+            debug = 1;
             while (n < argc) {
                 unsigned int x = atoi(argv[n++]);
-                cout << x << " is"
-                    << (SPRP_is_prime(x) ? " " : " not ")
-                    << "prime" << endl;
+                if (SPRP_is_prime(x)) {
+                    cout << x << " is prime" << endl;
+                    assert(prime_factoring(x) == 0);
+                } else {
+                    cout << x << ":";
+                    int count = prime_factoring(x);
+                    cout << " (" << count << ")" << endl;
+                }
             }
             return 0;
         }
     }
 
-    run();
+    run(max_num);
 
     return 0;
 }
-
